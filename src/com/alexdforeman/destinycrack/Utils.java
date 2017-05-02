20d7e2bbf43087289c292d1692f064e8ebe75d6c
Alex Foreman <alexdforeman@gmail.com>
2017-05-01 20:47:07 -0400

    Create Utils.java
    Initial Load

---
+++ src/com/alexdforeman/destinycrack/Utils.java
@@ -0,0 +1,20 @@
+package com.alexdforeman.destinycrack;
+
+/**
+ * Util methods.  Currently just has the CODE for future use.
+ * Created by @alexdforeman.
+ */
+public class Utils {
+
+    private static final char[] CODE = new char[] {'j', '?', '9', '6', 'e', 'x',
+            'd', '.', '?', 'i', ' ', '7',
+            'k', 'j', '\\', 'p', 's', 'q',
+            '3', 'e', 'u', 'j', '@', 'x',
+            '.', 'y', 't', '5', ' ', ',',
+            '3', '3', 's', 'z', 'l', '\'',
+            '!', '(', ' ', '+', '9', '-',
+            '7', '!', '!', '/', '@', '2',
+            'e', 'n', 'r', '?', 'h', 'q',
+            'j', '+', 'r', ',', 'r', '+',
+            'c', '?', ')'};
+}
